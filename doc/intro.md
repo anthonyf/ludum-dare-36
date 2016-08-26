245649b1601780c43474dd90484acf90b43bdddd
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-08-26 13:43:59 -0400

    Initial project created

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ludum-dare-36
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ludum_dare_36/core_test.clj
@@ -0,0 +1,7 @@
+(ns ludum-dare-36.core-test
+  (:require [clojure.test :refer :all]
+            [ludum-dare-36.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
